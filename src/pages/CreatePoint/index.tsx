a362c35dd861f399066427ae35c9d61e2b984708
Igor Matias <igorhmatias@gmail.com>
2020-06-07 22:45:35 -0300

    Done

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,9 +1,10 @@
-import React, {useEffect, useState, ChangeEvent} from 'react';
-import { Link } from 'react-router-dom'
-import { FiArrowLeft } from 'react-icons/fi'
-import {Map,TileLayer,Marker} from 'react-leaflet'
-import axios from 'axios'
-import api from '../../services/api'
+import React, {useEffect, useState, ChangeEvent, FormEvent} from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+import {Map,TileLayer,Marker} from 'react-leaflet';
+import axios from 'axios';
+import {LeafletMouseEvent} from 'leaflet';
+import api from '../../services/api';
 
 import  './styles.css';
 
@@ -27,9 +28,29 @@ const CreatePoint: React.FC = () => {
 
   const [items,setItems] = useState<Item[]>([]);
   const [ufs,setUfs] = useState<string[]>([]);
+  const [cities,setCities] = useState<string[]>([]);
+
+  const [initialPosition, setInitialPosition] = useState<[number,number]>([0,0]);
+
+  const [formData, SetFormData] = useState({
+    name:'',
+    email:'',
+    whatsapp:''
+  });
+
+  const [selectedItems,setSelectedItems] = useState<number[]>([]);
   const [selectedUf,setSelectedUf] = useState('0');
   const [selectedCity,setSelectedCity] = useState('0');
-  const [cities,setCities] = useState<string[]>([]);
+  const [selectedPosition, setSelectedPosition] = useState<[number,number]>([0,0]);
+  
+ const history = useHistory();
+
+  useEffect(()=>{
+    navigator.geolocation.getCurrentPosition(position =>{
+      const {latitude, longitude} = position.coords;
+      setInitialPosition([latitude, longitude]);
+    })
+  },[])
 
   useEffect(()=>{
     api.get('items').then(response=> {
@@ -56,14 +77,68 @@ const CreatePoint: React.FC = () => {
   function handleSelectuf(event: ChangeEvent<HTMLSelectElement>){
     const uf = event.target.value;
     setSelectedUf(uf)
+  
     
   }
   function handleSelectCity(event: ChangeEvent<HTMLSelectElement>){
     const city = event.target.value;
     setSelectedCity(city)
-    console.log(city);
     
+}
+
+function handleMapClick(event: LeafletMouseEvent){
+  setSelectedPosition([
+    event.latlng.lat,
+    event.latlng.lng
+  ]);
+
+}
+
+function handleInputChange(event: ChangeEvent<HTMLInputElement>){
+  const {name, value} = event.target;
+
+  SetFormData({...formData,[name]:value});
+
+}
+
+function handleSelectItem (id: number){
+  const alreadySelected = selectedItems.findIndex(item => item === id)
+
+  if (alreadySelected >= 0 ) {
+    const filteredItems = selectedItems.filter(item=> item !== id);
+    setSelectedItems(filteredItems);
+
+  } else {
+  setSelectedItems([...selectedItems,id]);
   }
+}
+
+async function handleSubmit(event: FormEvent){
+  event.preventDefault();
+
+  const { name, email, whatsapp} = formData;
+  const uf = selectedUf;
+  const city = selectedCity;
+  const [latitude, longitude] = selectedPosition
+  const items = selectedItems;
+    const data = {
+      name,
+      email,
+      whatsapp,
+      uf,
+      city,
+      latitude,
+      longitude,
+      items
+
+  }
+  await api.post('points', data)
+  alert('uhuuuuuuuuu');
+
+  history.push('/');
+
+
+}
   return (
     <div id="page-create-point">
       <header>
@@ -75,7 +150,7 @@ const CreatePoint: React.FC = () => {
         </Link>
       </header>
 
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <h1>Cadastro do <br/> ponto de coleta</h1>
 
         <fieldset>
@@ -89,6 +164,7 @@ const CreatePoint: React.FC = () => {
              type="text"
              name="name"
              id="name"
+             onChange={handleInputChange}
             />
             </div>
           <div className="field-group">
@@ -98,6 +174,7 @@ const CreatePoint: React.FC = () => {
              type="email"
              name="email"
              id="email"
+             onChange={handleInputChange}
             />
             </div>
           
@@ -107,6 +184,7 @@ const CreatePoint: React.FC = () => {
              type="text"
              name="whatsapp"
              id="whatsapp"
+             onChange={handleInputChange}
             />
             </div>
           </div>
@@ -117,12 +195,12 @@ const CreatePoint: React.FC = () => {
             <span>Selecionar o endereço no mapa</span>
           </legend>
 
-          <Map center={[-27.20902052, -49.6401092]} zoom={15}>
+          <Map center={initialPosition} zoom={15} onClick={handleMapClick}>
           <TileLayer
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />>
-        <Marker position={[-27.20902052, -49.6401092]} zoom={15}/>
+        <Marker position={selectedPosition} zoom={15}/>
           </Map>
           <div className="field-group">
             <div className="field">
@@ -143,10 +221,11 @@ const CreatePoint: React.FC = () => {
             <div className="field">
               <label htmlFor="uf">Cidade</label>
               <select 
+              onChange={handleSelectCity}
               name="city"
                id="city">
               value={selectedCity}
-              onChange={handleSelectCity}
+              
               <option value="0">Selecione uma cidade</option>
               {cities.map(city=>(
                   <option key={city}value={city}>{city}</option>
@@ -166,7 +245,9 @@ const CreatePoint: React.FC = () => {
 
           <ul className="items-grid">
             {items.map(item=> (
-               <li key={item.id}>
+               <li key={item.id} 
+               onClick={()=> handleSelectItem(item.id)}
+               className={selectedItems.includes(item.id)? 'selected' : ''}>
                <img src={item.image_url} alt={item.title}/>
             <span>{item.title}</span>
              </li>
@@ -180,5 +261,5 @@ const CreatePoint: React.FC = () => {
       </form>
     </div>
   )}
-
+            
 export default CreatePoint;
